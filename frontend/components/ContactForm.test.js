888097e7760f7e8334ba418f11949ac7702faf26
Jordan~ <jordan.brewer509@gmail.com>
2023-02-09 15:47:29 -0800

    bugged commit

--- frontend/components/ContactForm.test.js
+++ frontend/components/ContactForm.test.js
@@ -5,19 +5,44 @@ import userEvent from '@testing-library/user-event';
 import ContactForm from './ContactForm';
 
 test('renders without errors', () => {
-
+    render(<ContactForm />)
 });
 
 test('renders the contact form header', () => {
-
+    render(<ContactForm />);
+    const header = screen.getByText('Contact Form');
+    expect(header).toBeInTheDocument();
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
+    render(<ContactForm />);
+    const firstNameInput = screen.getByLabelText(/first name/i);
+    const firstNameErrMsg = screen.getByLabelText(/error/i);
+    userEvent.type(firstNameInput, 'Jane');
 
+    await waitFor(() => {
+        expect(firstNameErrMsg).toBeInTheDocument();
+    })
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
+    render(<ContactForm />);
+    
+    const firstNameInput = screen.getByLabelText(/first name/i);
+    const lastNameInput = screen.getByLabelText(/last name/i);
+    const emailInput = screen.getByLabelText(/email/i);
 
+    userEvent.type(firstNameInput, 'A');
+    userEvent.type(lastNameInput, 'B');
+    userEvent.type(emailInput, 'C');
+
+    fireEvent.keyDown(lastNameInput, {key: 'backspace'})
+
+    const threeErrMsgs = screen.getAllByText(/error/i)
+
+    await waitFor(() => {
+        expect(threeErrMsgs).toHaveLength(2)
+    })
 });
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
